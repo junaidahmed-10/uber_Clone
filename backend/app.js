85278f7471fbf7d873b5202c90309cfcb70a00e5
junaid <junaidsheikh45@gmail.com>
2025-01-06 01:30:09 +0530

    captain controller error fixed

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const app = express()
 const cookieParser = require('cookie-parser')
 const connectDB = require('./DB/db.js')
 const userRoutes = require('./routes/user.routes.js')
+const captainRoutes = require('./routes/captain.routes.js')
 
 connectDB()
 
@@ -19,5 +20,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/users', userRoutes)
+app.use('/captains', captainRoutes)
 
 module.exports = app
---
+++ backend/routes/captain.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator')
+const captainController = require('../controllers/captain.controller.js')
+
+router.post('/register', [
+    body('email').isEmail().withMessage('invalid email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('first name must be atleast 3 characters'),
+    body('password').isLength({min:6}).withMessage('password must be atleast 6 characters'),
+    body('vehicle.color').isLength({min:3}).withMessage('color must be atleast 3 characters'),
+    body('vehicle.plate').isLength({min:3}).withMessage('plate must be atleast 3 characters'),
+    body('vehicle.capacity').isInt({min:1}).withMessage('capacity must be atleast 1'),
+    body('vehicle.vehicleType').isIn('car', 'motorcycle', 'auto').withMessage('invalid')
+],
+    captainController.registerCaptain
+)
+
+module.exports = router;
